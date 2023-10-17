bcefbffbc795ae9de398f71a7c6d0d1ade24c400
Charan <bharicharanreddy@gmail.com>
2023-10-17 22:43:23 +0530

    test

--- app.py
+++ app.py
@@ -1,26 +1,23 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, Response
 import cv2
 import numpy as np
 import face_recognition
 import os
 from datetime import datetime
+import time
+import csv
 
 app = Flask(__name__)
 
-# Define the path to the directory containing images of people you want to recognize
 path = './images'  # Update this path to match your image directory
-
 images = []
 personNames = []
 myList = os.listdir(path)
-
-# Load images and extract person names from the file names
 for cu_img in myList:
     current_Img = cv2.imread(f'{path}/{cu_img}')
     images.append(current_Img)
     personNames.append(os.path.splitext(cu_img)[0])
 
-# Function to compute face encodings for a list of images
 def faceEncodings(images):
     encodeList = []
     for img in images:
@@ -29,60 +26,165 @@ def faceEncodings(images):
         encodeList.append(encode)
     return encodeList
 
-# Function to mark attendance
 def attendance(name):
-    with open('./Attendance.csv', 'r+') as f:
-        myDataList = f.readlines()
-        nameList = []
-        for line in myDataList:
-            entry = line.split(',')
-            nameList.append(entry[0])
-        if name not in nameList:
-            time_now = datetime.now()
-            tStr = time_now.strftime('%H:%M:%S')
-            dStr = time_now.strftime('%d/%m/%Y')
-            f.writelines(f'\n{name},{tStr},{dStr}')
+    with open('./Attendance.csv', 'a', newline='') as f:
+        time_now = datetime.now()
+        tStr = time_now.strftime('%H:%M:%S')
+        dStr = time_now.strftime('%d/%m/%Y')
+        writer = csv.writer(f)
+        writer.writerow([name, tStr, dStr])
 
 encodeListKnown = faceEncodings(images)
+print('All Encodings Complete!!!')
 
-@app.route('/', methods=['GET', 'POST'])
+cap = cv2.VideoCapture(0)
+
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return redirect(request.url)
+    last_attendance = {}
+    with open('./Attendance.csv', 'r', newline='') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            if len(row) == 3:  # Ensure that each row has three columns
+                last_attendance = {'image_name': row[0], 'time': row[1]}
 
-        file = request.files['file']
-        if file.filename == '':
-            return redirect(request.url)
+    return render_template('index.html', last_attendance=last_attendance)
 
-        if file:
-            # Read the uploaded image
-            image = cv2.imdecode(np.fromstring(file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
-            
-            # Process the uploaded image for facial recognition
-            facesCurrentFrame = face_recognition.face_locations(image)
-            encodesCurrentFrame = face_recognition.face_encodings(image, facesCurrentFrame)
+def gen():
+    last_attendance_update = time.time()
+    while True:
+        ret, frame = cap.read()
+        faces = cv2.resize(frame, (0, 0), None, 0.25, 0.25)
+        faces = cv2.cvtColor(faces, cv2.COLOR_BGR2RGB)
 
-            for encodeFace, faceLoc in zip(encodesCurrentFrame, facesCurrentFrame):
-                matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
-                faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-                matchIndex = np.argmin(faceDis)
+        facesCurrentFrame = face_recognition.face_locations(faces)
+        encodesCurrentFrame = face_recognition.face_encodings(faces, facesCurrentFrame)
+        current_time = time.time()
 
-                if matches[matchIndex]:
-                    name = personNames[matchIndex].upper()
+        for encodeFace, faceLoc in zip(encodesCurrentFrame, facesCurrentFrame):
+            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+            matchIndex = np.argmin(faceDis)
+
+            if matches[matchIndex]:
+                name = personNames[matchIndex].upper()
+                y1, x2, y2, x1 = faceLoc
+                y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.rectangle(frame, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+                cv2.putText(frame, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+
+                if current_time - last_attendance_update >= 30:  # 30 seconds
                     attendance(name)
+                    last_attendance_update = current_time
 
-    return render_template('index.html')
+            _, jpeg = cv2.imencode('.jpg', frame)
+            frame = jpeg.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True)
 
 
 
-# from flask import Flask
+# from flask import Flask, render_template, request, redirect, url_for, Response
+# import cv2
+# import numpy as np
+# import face_recognition
+# import os
+# from datetime import datetime
+# import time
+
 # app = Flask(__name__)
-# @app.route ('/')
+
+# path = './images'  # Update this path to match your image directory
+# images = []
+# personNames = []
+# myList = os.listdir(path)
+# print(myList)
+# for cu_img in myList:
+#     current_Img = cv2.imread(f'{path}/{cu_img}')
+#     images.append(current_Img)
+#     personNames.append(os.path.splitext(cu_img)[0])
+# print(personNames)
+
+# def faceEncodings(images):
+#     encodeList = []
+#     for img in images:
+#         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#         encode = face_recognition.face_encodings(img)[0]
+#         encodeList.append(encode)
+#     return encodeList
+
+# def attendance(name):
+#     with open('./Attendance.csv', 'a') as f:
+#         time_now = datetime.now()
+#         tStr = time_now.strftime('%H:M:S')
+#         dStr = time_now.strftime('%d/%m/%Y')
+#         f.writelines(f'\n{name},{tStr},{dStr}')
+
+# encodeListKnown = faceEncodings(images)
+# print('All Encodings Complete!!!')
+
+# cap = cv2.VideoCapture(0)
+
+# # Initialize the time for attendance update as a global variable
+# last_attendance_update = time.time()
+
+# @app.route('/')
 # def index():
-#     return "Hello World 🌍"
-# if __name__== "__main__":
+#     return render_template('index.html')
+
+# def gen():
+#     global last_attendance_update  # Declare it as a global variable
+#     while True:
+#         ret, frame = cap.read()
+#         faces = cv2.resize(frame, (0, 0), None, 0.25, 0.25)
+#         faces = cv2.cvtColor(faces, cv2.COLOR_BGR2RGB)
+
+#         facesCurrentFrame = face_recognition.face_locations(faces)
+#         encodesCurrentFrame = face_recognition.face_encodings(faces, facesCurrentFrame)
+
+#         current_time = time.time()  # Move this line here to ensure last_attendance_update is defined
+
+#         for encodeFace, faceLoc in zip(encodesCurrentFrame, facesCurrentFrame):
+#             matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+#             faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+#             # print(faceDis)
+#             matchIndex = np.argmin(faceDis)
+
+#             if matches[matchIndex]:
+#                 name = personNames[matchIndex].upper()
+#                 # print(name)
+#                 y1, x2, y2, x1 = faceLoc
+#                 y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+#                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+#                 cv2.rectangle(frame, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+#                 cv2.putText(frame, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+
+#                 # Check if it's time to update attendance
+#                 if current_time - last_attendance_update >= 30:  # 30 seconds
+#                     attendance(name)
+#                     last_attendance_update = current_time
+
+#         _, jpeg = cv2.imencode('.jpg', frame)
+#         frame = jpeg.tobytes()
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+# @app.route('/video_feed')
+# def video_feed():
+#     return Response(gen(),
+#                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# if __name__ == '__main__':
 #     app.run(debug=True)
+
+
+
